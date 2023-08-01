f492547b12b40a2bf000debe19bb2eca802e395b
Victor Esteve <viictoresteve11@gmail.com>
2023-08-01 13:29:00 +0200

    first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TriviaComponent } from './trivia/trivia.component';
+import { QuestonComponent } from './queston/queston.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TriviaComponent,
+    QuestonComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/trivia/trivia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trivia',
+  templateUrl: './trivia.component.html',
+  styleUrls: ['./trivia.component.css']
+})
+export class TriviaComponent {
+
+}
